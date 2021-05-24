687ec967b5c6acba3894323279946e7b1717f5b0
eolecvk <eolecvk@gmail.com>
2021-05-24 15:28:01 -0700

    userJoinRoom form

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -40,10 +40,71 @@ class CreateRoomForm extends React.Component {
         return (
             <form>
                 <h3>Create new room:</h3>
-                <label> Name: <input type="text" value={this.state.roomName} onChange={this.onChangeName} /></label>
-                <label> Amount (in cents): <input type="number" value={this.state.distributionCents} onChange={this.onChangeDistributionCents} /></label>
+                <label> Room name: <input type="text" required value={this.state.roomName} onChange={this.onChangeName} /></label>
+                <label> Amount (in cents): <input type="number" required min="1" value={this.state.distributionCents} onChange={this.onChangeDistributionCents} /></label>
                 <input type="submit" value="Submit" onSubmit={this.handleSubmit} />
-                <input type="submit" value="Cancel" onSubmit={this.handleCancel}/>
+                <input type="submit" value="Cancel" onSubmit={this.handleCancel} />
+            </form>
+        );
+    }
+}
+
+
+class JoinRoomForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { roomCode: '', userName: '', needMin: 0, needMax: 0 };
+        this.onChangeRoomCode = this.onChangeRoomCode.bind(this);
+        this.onChangeUserName = this.onChangeUserName.bind(this);
+        this.onChangeNeedMin = this.onChangeNeedMin.bind(this);
+        this.onChangeNeedMax = this.onChangeNeedMax.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleCancel = this.handleCancel.bind(this);
+    }
+
+
+    onChangeRoomCode = (event) => {
+        this.setState({ roomCode: event.target.value })
+    }
+
+    onChangeUserName = (event) => {
+        this.setState({ userName: event.target.value })
+    }
+
+    onChangeNeedMin = (event) => {
+        this.setState({ needMin: event.target.value })
+    }
+
+    onChangeNeedMax = (event) => {
+        this.setState({ needMax: event.target.value })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        const { roomCode, userName, needMin, needMax } = this.state
+        alert(`User session details: \n
+        room code: ${roomCode} \n 
+        username: ${userName} \n 
+        need min (cents): ${needMin} \n
+        need max (cents): ${needMax}`)
+    }
+
+    handleCancel = (event) => {
+        event.preventDefault()
+        this.state = { roomCode: '', userName: '', needMin: 0, needMax: 0 };
+        alert(`Returning to selection menu...`)
+    }
+
+    render() {
+        return (
+            <form>
+                <h3>Join a room:</h3>
+                <label> Room code: <input type="text" required value={this.state.roomCode} onChange={this.onChangeRoomCode} /></label>
+                <label> Username: <input type="text" required value={this.state.userName} onChange={this.onChangeUserName} /></label>
+                <label> Need min (cents): <input type="number" required min="0" value={this.state.needMin} onChange={this.onChangeNeedMin} /></label>
+                <label> Need max (cents): <input type="number" required min={this.state.needMin} value={this.state.needMax} onChange={this.onChangeNeedMax} /></label>
+                <input type="submit" value="Submit" onSubmit={this.handleSubmit} />
+                <input type="submit" value="Cancel" onSubmit={this.handleCancel} />
             </form>
         );
     }
@@ -54,7 +115,10 @@ class CreateRoomForm extends React.Component {
 function Home() {
     return (
         // <h1>Welcome to the Money Pot</h1>
-        <CreateRoomForm></CreateRoomForm>
+        <div>
+            <CreateRoomForm></CreateRoomForm>
+            <JoinRoomForm></JoinRoomForm>
+        </div>
     )
 }
 export default Home;
