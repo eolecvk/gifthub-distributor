fa2c0e43d69194fd67b09858ef0c68596f6f5e38
David M <davidmotson@gmail.com>
2021-05-28 15:16:33 -0700

    get rid of test in main.java

--- src/main/java/host/techcoop/gifthub/Main.java
+++ src/main/java/host/techcoop/gifthub/Main.java
@@ -12,12 +12,6 @@ public class Main {
   public static void main(String[] args) {
     Injector injector = Guice.createInjector(new GiftHubDistributorModule());
     GiftHubWebserver server = injector.getInstance(GiftHubWebserver.class);
-    Gson gson = injector.getInstance(Gson.class);
-    UpdateRequest request =
-        gson.fromJson(
-            "{\"events\":[{\"kind\": \"ADJUST\",\"bar_id\": 1234, \"new_value\": 0.5},{\"kind\": \"EMOTIVE\",\"emotion\":\"smiley face\",\"bar_id\": 1234 },{\"kind\":\"NEEDS_UPDATE\",\"needs_description\": \"i needs it\", \"needs_upper_bound_cents\": 1234,\"needs_lower_bound_cents\": 1234}]}",
-            UpdateRequest.class);
-    System.out.println(request);
     server.init();
   }
 }
