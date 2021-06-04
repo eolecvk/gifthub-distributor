05d4d2f4f7a5b3e29e32c5fd59ce6fef84b9abf1
eolecvk <eolecvk@gmail.com>
2021-06-03 23:05:23 -0700

    react helmet tags

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
+import { Helmet } from "react-helmet";
 import HomeMenu from './HomeMenu'
 
 
 function Home() {
     return (
-        <HomeMenu></HomeMenu>
+        <div className="home">
+            <Helmet>
+                <title>Home</title>
+                <meta
+                    name="viewport"
+                    content="minimum-scale=1, initial-scale=1, width=device-width"
+                />
+            </Helmet>
+            <HomeMenu />
+        </div>
     )
 }
 
