6d2bf35b099696726f598df5f7b5000aeb978b0a
eolecvk <eolecvk@gmail.com>
2021-06-06 11:54:59 -0700

    axios get params

--- frontend/src/Forms/JoinRoomForm.js
+++ frontend/src/Forms/JoinRoomForm.js
@@ -57,7 +57,7 @@ class JoinRoomForm extends Component {
 
 
         axios
-            .get(`/api/${roomCode}/join`, payload)
+            .get(`/api/${roomCode}/join`, { params: payload })
             .then(response => { console.log(response) })
             .catch(error => { console.log(error) });
 
