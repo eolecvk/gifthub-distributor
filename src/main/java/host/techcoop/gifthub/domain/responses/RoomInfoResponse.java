419397c63bd101606305792662effdc466b807ac
David M <davidmotson@gmail.com>
2021-06-06 12:30:05 -0700

    fixed output for user with no votes

--- src/main/java/host/techcoop/gifthub/domain/responses/RoomInfoResponse.java
+++ src/main/java/host/techcoop/gifthub/domain/responses/RoomInfoResponse.java
@@ -2,15 +2,12 @@ package host.techcoop.gifthub.domain.responses;
 
 import static com.google.common.collect.ImmutableList.toImmutableList;
 import static com.google.common.collect.ImmutableListMultimap.toImmutableListMultimap;
-import static java.util.stream.Collectors.toMap;
 
 import com.google.common.collect.ListMultimap;
 import host.techcoop.gifthub.domain.GiftHubRoom;
-import host.techcoop.gifthub.domain.User;
 import host.techcoop.gifthub.domain.UserVote;
 import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 import lombok.Builder;
 import lombok.Value;
 
@@ -24,20 +21,16 @@ public class RoomInfoResponse {
   List<UserResponse> people;
 
   public static RoomInfoResponse from(GiftHubRoom room) {
-    Map<Integer, User> usersById =
-        room.getPeople().stream().collect(toMap(User::getUserId, x -> x));
     ListMultimap<Integer, UserVote> votesByUserId =
         room.getPeople().stream()
             .flatMap(person -> person.getVotes().stream())
             .collect(toImmutableListMultimap(UserVote::getUserId, x -> x));
 
     List<UserResponse> userResponses =
-        votesByUserId.asMap().entrySet().stream()
+        room.getPeople().stream()
             .map(
-                (entry) -> {
-                  int userId = entry.getKey();
-                  Collection<UserVote> votes = entry.getValue();
-                  User user = usersById.get(userId);
+                user -> {
+                  Collection<UserVote> votes = votesByUserId.get(user.getUserId());
                   double averageVote =
                       votes.stream().mapToDouble(UserVote::getPercentSplit).average().orElse(0.0f);
                   return UserResponse.builder()
@@ -45,7 +38,7 @@ public class RoomInfoResponse {
                       .needsDescription(user.getNeedsDescription())
                       .needsLowerBoundCents(user.getNeedsLowerBoundCents())
                       .needsUpperBoundCents(user.getNeedsUpperBoundCents())
-                      .personId(userId)
+                      .personId(user.getUserId())
                       .voteTotal(averageVote)
                       .build();
                 })
