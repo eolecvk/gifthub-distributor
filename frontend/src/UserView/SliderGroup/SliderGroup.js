b23b7a62a53b3016181290311230faff7659d0f1
eolecvk <eolecvk@gmail.com>
2021-06-12 13:48:15 -0700

    rename InputSlider class

--- frontend/src/UserView/SliderGroup/SliderGroup.js
+++ frontend/src/UserView/SliderGroup/SliderGroup.js
@@ -1,11 +1,15 @@
+import { unstable_createMuiStrictModeTheme } from '@material-ui/core'
 import React from 'react'
-import InputSlider from '../Slider/Slider'
+import InputSlider from '../InputSlider/InputSlider'
 
 function SliderGroup(props) {
     return (
         props.people.map(userData =>
             <li key={userData.people_id.toString()}>
-                <InputSlider title={userData.name} />
+                <InputSlider
+                    title={userData.name}
+                    survive_amount={userData.needs_lower_bound_cents}
+                    thrive_amount={userData.needs_upper_bound_cents} />
             </li>
         )
     )
