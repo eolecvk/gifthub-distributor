ad4e3d3ecabe4b9708e4c76112611e5a898fbb08
tw85 <twessels85@gmail.com>
2021-07-06 22:39:33 -0500

    Victory api test

--- frontend/src/AdminView/AdminView.js
+++ frontend/src/AdminView/AdminView.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import AdminViewSlider from './AdminViewSlider';
 import { instanceOf } from 'prop-types';
 import { withCookies, Cookies } from 'react-cookie';
 import axios from 'axios';
+import {VictoryChart, VictoryBar, VictoryScatter }from 'victory';
+import colors from './../UserView/colors';
+import { quantile } from "./utils";
 
 // Example: https://blog.stvmlbrn.com/2019/02/20/automatically-refreshing-data-in-react.html
 class AdminView extends Component {
@@ -32,7 +34,7 @@ class AdminView extends Component {
             .get('/api/' + this.state.room_code)
             .then((response) => {
                 this.setState({ people: response.data.people });
-                // call getData() again in 5 seconds
+                // call getData() again in 1 second
                 this.intervalID = setTimeout(this.getData.bind(this), 1000);
             })
             .catch((error) => {
@@ -54,21 +56,32 @@ class AdminView extends Component {
                     ...people.map((p) => p.needs_upper_bound_cents) // flat list of upper bound needs in cents
                 )) / 100;
 
-        const AdminViewSliders = people
-            .sort((p1, p2) => p1.person_id - p2.person_id)
-            .map((p) => (
-                <AdminViewSlider
-                    key={'adminviewslider' + p.person_id.toString()}
-                    sliderId={p.person_id.toString()}
-                    name={p.name}
-                    needsUpperBound={p.needs_upper_bound_cents / 100}
-                    needsLowerBound={p.needs_lower_bound_cents / 100}
-                    totalAmountDollars={totalAmountDollars}
-                    max={max}
-                    votes={p.votes_cents.map((v) => v / 100)}
-                    avg={p.avg_cents / 100}
+        const data = people.map((p, index) => {
+            const name = p.name;
+            const cents = p.avg_cents/100;
+            const needs_upper = p.needs_upper_bound_cents/100;
+            const needs_lower = p.needs_lower_bound_cents/100;
+            const upper_25 = quantile(p.votes_cents,0.75)/100;
+            const lower_25 = quantile(p.votes_cents,0.25)/100;
+            return {name:name, cents:cents, needs_upper:needs_upper, needs_lower:needs_lower, upper_25:upper_25, lower_25:lower_25, color:colors[index]};
+        });
+
+        const barchart = (
+            <VictoryChart domainPadding={20} height={150} padding={{ top: 0, bottom: 50, left: 75, right: 25 }}>
+                <VictoryBar
+                    barRatio={4/data.length}
+                    style={{ data: {fill: ({ datum }) => datum.color }}}
+                    data={data}
+                    x = "name"
+                    y = "cents"
+                    horizontal = {true}
                 />
-            ));
+                <VictoryScatter data={data} symbol = "circle" x = "name" y = "needs_upper"  horizontal = {true} style={{ data: { fill: "#00FF00" } }}/>
+                <VictoryScatter data={data} symbol = "circle" x = "name" y = "needs_lower"  horizontal = {true} style={{ data: { fill: "#FF0000" } }}/>
+                <VictoryScatter data={data} symbol = "cross" x = "name" y = "upper_25"  horizontal = {true} style={{ data: { fill: "#000000" } }}/>
+                <VictoryScatter data={data} symbol = "cross" x = "name" y = "lower_25"  horizontal = {true} style={{ data: { fill: "#000000" } }}/>
+            </VictoryChart>
+        );
 
         return (
             <div>
@@ -76,7 +89,9 @@ class AdminView extends Component {
                 <h2>{roomName}</h2>
                 <h2>Room Code: {roomCode}</h2>
                 <h2>Total Amount: ${totalAmountDollars}</h2>
-                {AdminViewSliders}
+                <div>
+                    {barchart}
+                </div>
             </div>
         );
     }
---
+++ frontend/src/AdminView/utils.js
@@ -0,0 +1,33 @@
+
+function asc(arr){
+    return arr.sort((a, b) => a - b);
+}
+
+function sum(arr){
+    return arr.reduce((a, b) => a + b, 0);
+}
+
+function mean(arr){
+    return sum(arr) / arr.length;
+}
+
+// sample standard deviation
+function std(arr) {
+    const mu = mean(arr);
+    const diffArr = arr.map(a => (a - mu) ** 2);
+    return Math.sqrt(sum(diffArr) / (arr.length - 1));
+}
+
+function quantile(arr, q) {
+    const sorted = asc(arr);
+    const pos = (sorted.length - 1) * q;
+    const base = Math.floor(pos);
+    const rest = pos - base;
+    if (sorted[base + 1] !== undefined) {
+        return sorted[base] + rest * (sorted[base + 1] - sorted[base]);
+    } else {
+        return sorted[base];
+    }
+}
+
+export {quantile};
