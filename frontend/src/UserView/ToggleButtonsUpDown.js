7d1a9dc08c9e9b07a9bd6ac55066f9e95d0c7d5f
eolecvk <eolecvk@gmail.com>
2021-07-17 15:26:38 -0500

    localStorage keeps track of toggle state for all bars in opinions object

--- frontend/src/UserView/ToggleButtonsUpDown.js
+++ frontend/src/UserView/ToggleButtonsUpDown.js
@@ -11,14 +11,20 @@ import { registerEmotive } from './utils';
 // Test 
 
 function ToggleButtonsUpDown(props) {
-  const { sliderId } = props
-  const storedOpinion = sessionStorage.getItem("opinion") || ''
 
-  const [opinion, setOpinion] = React.useState(storedOpinion);
+  const { sliderId } = props
   const roomInfo = JSON.parse(sessionStorage.getItem("roomInfo"))
   const roomCode = roomInfo.room_code
 
+  //Access stored opinions in localStorage
+  //and set state to existing value if applicable
+  const storedOpinions = JSON.parse(sessionStorage.getItem("opinions")) || {}
+  const barsWithOpinions = Object.keys(storedOpinions)
+  const storedOpinion = barsWithOpinions.includes(sliderId) ? storedOpinions[sliderId] : ''
+  const [opinion, setOpinion] = React.useState(storedOpinion);
+
   const handleOpinion = (event, newOpinion) => {
+
     let emotiveChange = {}
     emotiveChange['kind'] = 'EMOTIVE'
     emotiveChange['bar_id'] = sliderId
@@ -26,8 +32,15 @@ function ToggleButtonsUpDown(props) {
     emotiveChange['toggle'] = newOpinion ? 'ON' : 'OFF'
     const events = [emotiveChange]
 
-    sessionStorage.setItem("opinion", newOpinion)
+    //Update stored emotion by bar_id in sessionStorage
+    let opinionsUpdated = storedOpinions
+    opinionsUpdated[sliderId] = newOpinion
+    sessionStorage.setItem("opinions", JSON.stringify(opinionsUpdated))
+
+    //Send request to backend to update emotive data
     registerEmotive(events, roomCode)
+
+    // Update component state
     setOpinion(newOpinion);
   };
 
