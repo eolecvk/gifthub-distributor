9897977bd269eed0f7e7b36fee81851e1d731011
eolecvk <eolecvk@gmail.com>
2021-09-15 11:08:14 -0700

    recipient CSV upload and PUT RECIPIENT_ADD

--- frontend/src/Admin/EditRoomModal.js
+++ frontend/src/Admin/EditRoomModal.js
@@ -1,38 +1,21 @@
 import React from 'react';
-import { Container } from '@material-ui/core';
 import EditRoomForm from './EditRoomForm';
-import CustomButton from '../CustomButton';
 import CustomModal from '../CustomModal';
-import EditIcon from '@material-ui/icons/Edit';
 
 function EditRoomModal(props) {
     const [open, setOpen] = React.useState(false);
-
-    const handleOpen = () => {
-        setOpen(true);
-    };
 
     const handleClose = () => {
         setOpen(false);
     };
 
     return (
-        <div>
-            <CustomButton
-                title="Edit room config"
-                startIcon={<EditIcon />}
-                size="medium"
-                onClick={handleOpen}
-            />
-            <Container>
-                <CustomModal
-                    title="Edit Room Info"
-                    form={<EditRoomForm handleClose={handleClose} roomCode={props.roomCode} />}
-                    show={open}
-                    handleClose={handleClose}
-                />
-            </Container>
-        </div>
+        <CustomModal
+            title="Edit Room Info"
+            form={<EditRoomForm handleClose={handleClose} roomCode={props.roomCode} />}
+            show={open}
+            handleClose={handleClose}
+        />
     );
 }
 
--- frontend/src/Admin/RecipientsManagement.js
+++ frontend/src/Admin/RecipientsManagement.js
@@ -1,22 +1,88 @@
 import React from 'react';
 import CustomButton from '../CustomButton';
 import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import ReactFileReader from 'react-file-reader';
+import Papa from 'papaparse';
+import axios from 'axios';
 
-function RecipientsManagement() {
-    const handleUploadRecipients = () => {
-        console.log('Upload Recipients');
+function RecipientsManagement(props) {
+    const { roomCode } = props;
+
+    const registerRecipients = (recipients) => {
+        // Verify input file format
+        const headers = Object.keys(recipients[0]);
+        const requiredHeaders = ['name', 'survive', 'thrive', 'description'];
+        let missingHeaders = [];
+        requiredHeaders.forEach((requiredHeader) => {
+            if (!headers.includes(requiredHeader)) {
+                missingHeaders.push(requiredHeader);
+            }
+        });
+
+        if (missingHeaders.length > 0) {
+            alert(`Invalid input file format, missing headers: ${missingHeaders.toString()}`);
+            return;
+        }
+
+        const events = recipients
+            .filter((row) => {
+                if (row['name'] === '') {
+                    return false;
+                }
+                return true;
+            })
+            .map((recipient) => {
+                const name = recipient['name'];
+                const description = recipient['description'];
+                const surviveCents = parseInt(recipient['survive']) * 100;
+                const thriveCents = parseInt(recipient['thrive']) * 100;
+
+                return {
+                    kind: 'RECIPIENT_ADD',
+                    name: name,
+                    needs_upper_bound_cents: surviveCents,
+                    needs_lower_bound_cents: thriveCents,
+                    needs_description: description,
+                };
+            });
+
+        const payload = { events: events };
+        axios
+            .put(`/api/${roomCode}`, payload)
+            .then((response) => {
+                if (response.status === 200) {
+                    sessionStorage.setItem('roomInfo', JSON.stringify(response.data));
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
+    const handleUploadRecipients = (files) => {
+        Papa.parse(files[0], {
+            complete: function (res) {
+                registerRecipients(res.data);
+            },
+            header: true,
+        });
     };
 
     return (
         <div>
             <h3>Recipients</h3>
             <div style={{ margin: 15 + 'px' }}>
-                <CustomButton
-                    title="Upload recipients data"
-                    startIcon={<CloudUploadIcon />}
-                    size="medium"
-                    onClick={handleUploadRecipients}
-                />
+                <ReactFileReader
+                    handleFiles={handleUploadRecipients}
+                    fileTypes={'.csv'}
+                    multipleFiles={false}
+                >
+                    <CustomButton
+                        title="Upload recipients data"
+                        startIcon={<CloudUploadIcon />}
+                        size="medium"
+                    />
+                </ReactFileReader>
             </div>
         </div>
     );
