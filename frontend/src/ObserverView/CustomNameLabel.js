f563cd19be1a28a2a40d39e87992723f4193a789
twessels85 <twessels85@gmail.com>
2021-09-15 19:23:41 -0500

    Deleting leftover imports

--- frontend/src/ObserverView/CustomNameLabel.js
+++ frontend/src/ObserverView/CustomNameLabel.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { withStyles, makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
 import Tooltip from '@material-ui/core/Tooltip';
-import Typography from '@material-ui/core/Typography';
 
 const HtmlTooltip = withStyles((theme) => ({
     tooltip: {
