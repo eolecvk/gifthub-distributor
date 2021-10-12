b07eec6fd884e592869aac88dce151bb26658d24
David M <davidmotson@gmail.com>
2021-10-12 16:43:33 -0700

    added tests, and first dummy test

---
+++ src/test/java/host/techcoop/gifthub/WordGeneratorTest.java
@@ -0,0 +1,12 @@
+package host.techcoop.gifthub;
+
+import static com.google.common.truth.Truth.*;
+import org.junit.Test;
+
+public class WordGeneratorTest {
+
+  @Test
+  public void shouldAlwaysPass() {
+    assertThat(5).isEqualTo(5);
+  }
+}
