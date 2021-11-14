117ef5ddd9ae7965bb1dc8a6bf3ea7291f434204
David M <davidmotson@gmail.com>
2021-11-13 16:47:56 -0800

    improved the violin plot by generating a high res histogram

--- frontend/src/ObserverView/ViolinBarLine.js
+++ frontend/src/ObserverView/ViolinBarLine.js
@@ -6,15 +6,9 @@ const ViolinBarLine = (props) => {
 
     var x = d3.scaleLinear().domain(props.x_domain).range([0, props.width]);
 
-    var histogram = d3
-        .histogram()
-        .domain(x.domain())
-        .thresholds(x.ticks(30))
-        .value((d) => d);
+    var binnedValues = highResHistogram(data, 200, props.x_domain[1]);
 
-    var binnedValues = histogram(data);
-
-    var maxY = d3.max(binnedValues.map((bin) => bin.length));
+    var maxY = d3.max(binnedValues.map((bin) => bin[0]));
 
     var heightScale = d3.scaleLinear().range([0, props.height]).domain([-maxY, maxY]);
 
@@ -24,14 +18,49 @@ const ViolinBarLine = (props) => {
             return x(d.x0) + props.x;
         })
         .y0(function (d) {
-            return heightScale(-d.length) + props.y;
+            return heightScale(-d[0]) + props.y;
         })
         .y1(function (d) {
-            return heightScale(d.length) + props.y;
+            return heightScale(d[0]) + props.y;
         })
         .curve(d3.curveMonotoneX);
 
     return <path d={violin(binnedValues)} stroke="none" fill={props.bar_fill} />;
 };
+
+function highResHistogram(data, ticks, domain) {
+    const histogram = [];
+    for (let i = 0; i < ticks; i++) {
+        histogram.push([0]);
+        histogram[i].x0 = Math.floor((domain * i) / ticks);
+        histogram[i].x1 = Math.floor((domain * (i + 1)) / ticks);
+    }
+    for (let d = 0; d < data.length; d++) {
+        const point = data[d];
+        var adjustedPoint = Math.floor((point * ticks) / domain);
+        console.log(adjustedPoint);
+        histogram[adjustedPoint][0] += 6;
+
+        if (adjustedPoint > 0) {
+            histogram[adjustedPoint - 1][0] += 5;
+        }
+        if (adjustedPoint < ticks - 1) {
+            histogram[adjustedPoint + 1][0] += 5;
+        }
+        if (adjustedPoint > 1) {
+            histogram[adjustedPoint - 2][0] += 3;
+        }
+        if (adjustedPoint < ticks - 2) {
+            histogram[adjustedPoint + 2][0] += 3;
+        }
+        if (adjustedPoint > 2) {
+            histogram[adjustedPoint - 3][0] += 1;
+        }
+        if (adjustedPoint < ticks - 3) {
+            histogram[adjustedPoint + 3][0] += 1;
+        }
+    }
+    return histogram;
+}
 
 export default ViolinBarLine;
